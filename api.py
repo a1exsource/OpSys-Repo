425834450c053f9eb8bb2a8d06bbf9d08417f7c6
alex.neu <alex.nachtech@gmail.com>
2025-11-08 17:04:13 +0100

    Add files via upload

---
+++ api.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  3 18:09:34 2025
+
+ - Operational Systems Repository - OpSys-Repo -
+ -                  API code                   -
+ 
+ Tracking NATO military systems, suppliers, and materials.
+ 
+ 
+ Purely for educational purposes.
+ Everything is from public and legal sources.
+ 
+@author: a1exsource
+"""
+
---
+++ seed_db.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  3 18:09:34 2025
+
+ - Operational Systems Repository - OpSys-Repo -
+ -              Database code                  -
+ 
+ Tracking NATO military systems, suppliers, and materials.
+ 
+ 
+ Purely for educational purposes.
+ Everything is from public and legal sources.
+ 
+@author: a1exsource
+"""
+import json
+with open("db.json") as f:
+    data = json.load(f)
+
+
+
+
+
